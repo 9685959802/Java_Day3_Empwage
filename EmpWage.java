8ac4a26257a71d9482777603e3c7b1e220b1711c
9685959802 <tusharbadgujar0000@gmail.com>
2022-09-05 21:21:27 +0530

    Welcome to Employee Wage Program

---
+++ EmpWage.java
@@ -0,0 +1,14 @@
+package Brigdelabz.java.empwage;
+
+import java.util.Random;
+
+public class EmpWage {
+	public static void main(String[] args) {
+		
+		//Master-Branch
+		System.out.println("Welcome to Employee Wage Computation Program :)");
+				
+		        
+	}
+
+}
