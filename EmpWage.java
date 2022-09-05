a6ae8625fb7f7c1cc79941f233350ec5461ae66a
9685959802 <tusharbadgujar0000@gmail.com>
2022-09-05 21:36:31 +0530

    Check employee present or absent

--- EmpWage.java
+++ EmpWage.java
@@ -8,7 +8,15 @@ public class EmpWage {
 		//Master-Branch
 		System.out.println("Welcome to Employee Wage Computation Program :)");
 				
-		        
+		//UC-1-Check Employee is Present or Absent
+				Random empCheck = new Random();
+		     int n = empCheck.nextInt(2);
+		     if (n == 1)
+				System.out.println("Employee is Present");
+		     else
+				System.out.println("Employee is Absent");
+				        
+ 
 	}
 
 }
